f5281a40ad352fff3fdd77ea06437d846245a776
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-09 10:07:52 +0600

    feat(counter): create counter application and state mange by redux

---
+++ script.js
@@ -0,0 +1,49 @@
+// select dom element
+const counterEl = document.getElementById("counter");
+const incrementEl = document.getElementById("increment");
+const decrementEl = document.getElementById("decrement");
+
+// initial state
+const initialState = {
+  value: 0,
+};
+
+// create reducer function
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increment") {
+    return {
+      ...state,
+      value: state.value + 1,
+    };
+  } else if (action.type === "decrement") {
+    return {
+      ...state,
+      value: state.value - 1,
+    };
+  } else {
+    return state;
+  }
+};
+
+// store
+const store = Redux.createStore(counterReducer);
+
+const render = () => {
+  const state = store.getState();
+  counterEl.innerText = state.value;
+};
+
+store.subscribe(render);
+
+// event listiner
+incrementEl.addEventListener("click", () => {
+  store.dispatch({
+    type: "increment",
+  });
+});
+
+decrementEl.addEventListener("click", () => {
+  store.dispatch({
+    type: "decrement",
+  });
+});
