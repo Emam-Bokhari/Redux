eacb8c03b9c5b673c0d7a6be65b4ee9d56133e8d
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-12 07:59:37 +0600

    feat(counter): implement reducer function,actions,action creators for counter increment and decrement

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+export default function App() {
+  return (
+    <Fragment>
+      <div>
+        <p>Counter: 0</p>
+        <button>Increment</button>
+        <button>Decrement</button>
+      </div>
+    </Fragment>
+  );
+}
