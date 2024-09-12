973e95616e0d6b935ab9e7ccb371356727240743
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-12 10:21:50 +0600

    feat(middleware): create simple custom logger middleware by applyMiddleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,13 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+const logger=(store)=>(next)=>(action)=>{
+    console.log(`Action:${JSON.stringify(action)}`);
+    console.log(`Before:${JSON.stringify(store.getState())}`);
+    
+    return next(action)
+}
+
+const store = createStore(rootReducer,applyMiddleware(logger));
 
 export default store;
