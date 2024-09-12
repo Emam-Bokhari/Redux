ead24ba2c281f33dd681a9b63db1b165c459ace8
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-12 12:07:50 +0600

    feat(devtools): add redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,11 @@ import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./rootReducer";
 // import logger from "./middleware/logger";
 import logger from "redux-logger";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
