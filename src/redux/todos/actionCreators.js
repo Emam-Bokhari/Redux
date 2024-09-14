de74c28a43fc2b409247007ba86ca09488c01d94
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-14 11:54:05 +0600

    feat(todo-app): add Redux action creators for todo operations - added action creators for adding,deleting,and toggling todos

---
+++ src/redux/todos/actionCreators.js
@@ -0,0 +1,53 @@
+import {
+  ADDED,
+  ALLCOMPLETED,
+  CLEARCOMPLETED,
+  COLORSELECTED,
+  DELETED,
+  TOGGLED,
+} from "./actionTypes";
+
+export const added = (todos) => {
+  return {
+    type: ADDED,
+    payload: todos,
+  };
+};
+
+export const deleted = (todoId) => {
+  return {
+    type: DELETED,
+    payload: { todoId },
+  };
+};
+
+export const toggled = (todoId) => {
+  return {
+    type: TOGGLED,
+    payload: {
+      todoId,
+    },
+  };
+};
+
+export const colorSelected = (color, todoId) => {
+  return {
+    type: COLORSELECTED,
+    payload: {
+      color,
+      todoId,
+    },
+  };
+};
+
+export const allCompleted = () => {
+  return {
+    type: ALLCOMPLETED,
+  };
+};
+
+export const clearCompleted = () => {
+  return {
+    type: CLEARCOMPLETED,
+  };
+};
