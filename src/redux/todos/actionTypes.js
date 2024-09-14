6ee1647968c2639430b582f3d8c626818080945d
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-14 11:42:33 +0600

    feat(todos-action types): create todos action types (ADDED,ALLCOMPLETED,CLEARCOMPLETED,DELETED,COLORSELECTED,TOGGLED)

---
+++ src/redux/todos/actionTypes.js
@@ -0,0 +1,6 @@
+export const ADDED = "todos/added";
+export const ALLCOMPLETED = "todos/Allcompleted";
+export const CLEARCOMPLETED = "todos/clearCompleted";
+export const DELETED = "todos/deleted";
+export const COLORSELECTED = "todos/colorSelected";
+export const TOGGLED = "todos/toggled";
