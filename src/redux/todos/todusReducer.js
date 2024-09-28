386f0858cf568a464c3aed27c44f14ff19191338
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-28 19:40:30 +0600

    feat(reducer): implement todo actions in reducer (toggle, delete, color select, complete all, clear completed)

---
+++ src/redux/todos/todusReducer.js
@@ -0,0 +1,56 @@
+import {
+  ADDED,
+  ALLCOMPLETED,
+  CLEARCOMPLETED,
+  COLORSELECTED,
+  DELETED,
+  TOGGLED,
+} from "./actionTypes";
+import { initialState } from "./initialState";
+
+const todosReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADDED:
+      return [
+        ...state,
+        {
+          id: crypto.randomUUID(),
+        },
+      ];
+    case TOGGLED:
+      return state.map((todo) => {
+        if (todo.id !== action.payload) {
+          return todo;
+        }
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      });
+    case COLORSELECTED: {
+      const { color, todoId } = action.payload;
+      return state.map((todo) => {
+        if (todo.id !== todoId) {
+          return todo;
+        }
+        return {
+          ...todo,
+          color: color,
+        };
+      });
+    }
+    case DELETED:
+      return state.filter((todo) => todo.id !== action.payload);
+    case ALLCOMPLETED:
+      return state.map((todo) => {
+        return {
+          ...todo,
+          completed: true,
+        };
+      });
+    case CLEARCOMPLETED:
+      return state.filter((todo) => !todo.completed);
+  }
+};
+
+export default todosReducer;
